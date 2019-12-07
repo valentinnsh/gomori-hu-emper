047e4f66e16dbd3ce99c96fa7ed5ed1a040fc118
valentinnsh <valentinnsh@gmail.com>
2019-12-07 21:50:36 +0300

    Done, but didnt work out. to fix

--- algorithm.py
+++ algorithm.py
@@ -56,14 +56,18 @@ def find_connected_component(G, s,t):
 
 def build_gomory_hu_tree(G0):
     T = nx.Graph() # Результирующее дерево
-    T.add_node(tuple(G0.nodes())) # На первом шаге у Т только 1 вершина
+    new_nodes = []
+    for i in G0.nodes():
+        new_nodes.append(tuple([i]))
+
+    T.add_node(tuple(new_nodes)) # На первом шаге у Т только 1 вершина
     while True:
         # Шаг 2 - выбор группы вершин из V(t) в которой больше 1 вершины
         # Если все группы по одной вершине - дерево готово
         X = ()
         for n in T.nodes():
             if len(n) >= 2:
-                X = tuple(n)
+                X = n
         if not X:
             break
 
@@ -71,48 +75,88 @@ def build_gomory_hu_tree(G0):
         G = nx.Graph()
         # Сначала добавляем Х целиком
         for i in range(len(X)):
-            G.add_node(tuple([X[i]]))
-        T.add_node(tuple([99])); T.add_node(tuple([777]))
-        T.add_edge(X,tuple([99]), capacity = 1); T.add_edge(tuple([777]),tuple([99]), capacity = 2);
+            G.add_node(X[i])
+
+        # -----------------for test---------------------
+       # T.add_node(tuple([99])); T.add_node(tuple([777]))
+        #T.add_edge(X,tuple([99]), capacity = 1); T.add_edge(tuple([777]),tuple([99]), capacity = 2);
+        #------------------for test---------------------
+
+
         # Потом добвляем сгруппированные сеты вершин компонент связности T\X
         for i in T.neighbors(X):
+            show_tree(T, 'test.png')
             G.add_node(tuple(find_connected_component(T,i,X)))
 
 
         for u in X:
             for v in X:
-                if G0.has_edge(u,v):
-                    G.add_edge(tuple([u]),tuple([v]), capacity = G0.edges[u,v]['capacity'])
+                if G0.has_edge(u[0],v[0]):
+                    G.add_edge(u,v, capacity = G0.edges[u[0],v[0]]['capacity'])
 
         # фиксануть веса ребер в G
         for n in G.nodes():
-            if n[0] not in X:
+            if n not in X:
                 for u in X:
                     total_cap = 0
                     for v in n:
 
-                        if G0.has_edge(u,v[0]) and tuple([u]) not in n:
-                            total_cap += G0.edges[u,v[0]]['capacity']
+                        if G0.has_edge(u[0],v[0]) and u not in n:
+                            print('tolal cap changed')
+                            total_cap += G0.edges[u[0],v[0]]['capacity']
+                            print(total_cap)
                     if total_cap:
-                        G.add_edge(tuple([u]),n)
-                        G.edges[tuple([u]),n]['capacity'] = total_cap
+                        G.add_edge(u,n)
+                        G.edges[u,n]['capacity'] = total_cap
 
-        show_tree(G,'test.png')
+        # show_tree(G,'test.png')
 
 
         # Шаг 4 Ищем минимальный st-разрез в G
-        s = tuple([X[0]]); t = tuple([X[1]])
+        # А также строим множества А и В
+        s = X[0]; t = X[1]
         print(s,t)
-        A = tuple(find_connected_component(G,t,s))
-        B = tuple(find_connected_component(G,s,t))
+        A = list(find_connected_component(G,t,s))
+        B = list(find_connected_component(G,s,t))
 
         cut_value, cutset = find_min_st_cut(G, s, t)
 
         G1 = G.copy()
         G1.remove_edges_from(list(cutset))
-        print(tuple(G.edges()))
         A = tuple(find_connected_component(G1,t,s))
         B = tuple(find_connected_component(G1,s,t))
+
+
+        # Шаг 5 перестраиваем дерево T
+        # new_T = nx.Graph()
+
+        new_T_nodes = []
+
+        new_T_nodes.append(tuple(set(T) - set(X)))
+        new_T_nodes.append(tuple(set(A)&set(X)))
+        new_T_nodes.append(tuple(set(B)&set(X)))
+        new_T = nx.Graph()
+        new_T.add_nodes_from(new_T_nodes)
+        # show_tree(T, 'test.png')
+        # show_tree(new_T, 'test.png')
+
+        new_T_edges = []
+        for y in T.neighbors(X):
+            if y in A:
+                e1 =(tuple(set(A)&set(X)), y)
+            else:
+                e1 =(tuple(set(B)&set(X)), y)
+            w = T.edges[X,y]['capacity']
+            new_T.add_edge(e1[0], e1[1], capacity = w)
+        new_T.add_edge(tuple(set(A)&set(X)), tuple(set(B)&set(X)), capacity = cut_value)
+
+        show_tree(new_T, 'tree.png')
+
+        T = new_T.copy()
+
+
+
+
 
 def main():
 
